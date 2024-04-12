9d0143288e29e48709462802f28e2a5b52b15372
Swasti Rai <80086541+Aviz17@users.noreply.github.com>
2024-04-12 15:15:34 +0530

    Update aim.py

--- aim.py
+++ aim.py
@@ -7,7 +7,7 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Aim Trainer")
+pygame.display.set_caption("Aim Labs")
 
 TARGET_INCREMENT = 400
 TARGET_EVENT = pygame.USEREVENT
